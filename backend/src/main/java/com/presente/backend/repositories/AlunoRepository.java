4a4d8b4820be4f583955511f7866d353cd053889
Charlles Bandeira <charllesbandeira@gmail.com>
2019-01-25 18:43:29 -0200

    transaction read only repositories

--- backend/src/main/java/com/presente/backend/repositories/AlunoRepository.java
+++ backend/src/main/java/com/presente/backend/repositories/AlunoRepository.java
@@ -4,14 +4,18 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.presente.backend.domains.Aluno;
 
 @Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
  
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 	Optional<Aluno> findByMatriculaAndAnoLetivoAndIdNot(String matricula, Integer anoLetivo, Integer id);
 
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 	Optional<Aluno> findByMatriculaAndAnoLetivo(String matricula, Integer anoLetivo);
 
 }
--- backend/src/main/java/com/presente/backend/repositories/LogAlteracaoAlunoRepository.java
+++ backend/src/main/java/com/presente/backend/repositories/LogAlteracaoAlunoRepository.java
@@ -4,12 +4,15 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.presente.backend.domains.LogAlteracaoAluno;
 
 @Repository
 public interface LogAlteracaoAlunoRepository extends JpaRepository<LogAlteracaoAluno, Integer> {
 
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 	Optional<LogAlteracaoAluno> findFirstByMatriculaAndAnoLetivoAndAtivoOrderByDataUltimaAtualizacaoDesc(
 			String matricula, Integer anoLetivo, Boolean ativo);
 
