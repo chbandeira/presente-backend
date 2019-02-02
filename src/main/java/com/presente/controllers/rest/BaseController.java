6a7c96db7cc8b1af32f0c86f03c99a4c122e2df1
Charlles Bandeira <charllesbandeira@gmail.com>
2019-02-02 00:18:43 -0200

    permission crossorigin

--- src/main/java/com/presente/controllers/rest/BaseController.java
+++ src/main/java/com/presente/controllers/rest/BaseController.java
@@ -1,8 +1,9 @@
 package com.presente.controllers.rest;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@CrossOrigin(origins = "${default.cross-origin}")
+@CrossOrigin(origins = "${default.cross-origin}", methods = {RequestMethod.PUT, RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE})
 public class BaseController {
 
 }
